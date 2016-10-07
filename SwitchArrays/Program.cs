4ca14c839d302ef327e5f6f3f651c92d193fef2c
Gordon Beeming <gordon@beeming.co.za>
2016-10-07 22:51:09 +0200

    init commit

--- SwitchArrays/Program.cs
+++ SwitchArrays/Program.cs
@@ -10,6 +10,67 @@ namespace SwitchArrays
     {
         static void Main(string[] args)
         {
+            int[,] array1 = new int[6, 8];
+            PopulateArray(array1);
+            PrintArray(array1);
+            int[,] array2 = CopyArray(array1, 2, 0, 4, 6);
+            PrintArray(array2);
+
+            
+            Console.WriteLine("Done!");
+            Console.ReadLine();
+        }
+
+        private static int[,] CopyArray(int[,] array, int fromX, int fromY, int lengthX, int lengthY)
+        {
+            int[,] result = new int[lengthX, lengthY];
+            for (int x = 0; x < result.GetLength(0); x++)
+            {
+                for (int y = 0; y < result.GetLength(1); y++)
+                {
+                    result[x, y] = array[x + fromX, y + fromY];
+                }
+            }
+            return result;
+        }
+
+        private static int[,] PopulateArray(int[,] array)
+        {
+            var rdn = new Random(DateTime.Now.Millisecond);
+            for (int x = 0; x < array.GetLength(0); x++)
+            {
+                for (int y = 0; y < array.GetLength(1); y++)
+                {
+                    array[x, y] = rdn.Next(0, 10000);
+                }
+            }
+            return array;
+        }
+
+        private static void PrintArray(int[,] array)
+        {
+            for (int x = 0; x < array.GetLength(0); x++)
+            {
+                for (int y = 0; y < array.GetLength(1); y++)
+                {
+                    Console.Write($"{FormatTo(array[x, y], 4)} ");
+                }
+                Console.WriteLine();
+            }
+            Console.WriteLine();
+            Console.WriteLine();
+        }
+
+        private static string FormatTo(int value, int digits)
+        {
+            string result = value.ToString();
+            digits -= result.Length;
+            while (digits > 0)
+            {
+                result = " " + result;
+                digits--;
+            }
+            return result;
         }
     }
 }
